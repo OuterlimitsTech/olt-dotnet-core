595c605918e609c8d83d3fc9a8a2d0fef6df75ee
Chris Straw <chris.straw@outerlimitstech.com>
2022-02-27 19:52:38 -0500

    code coverage

--- tests/OLT.EF/OLT.EF.Common.Tests/EntityModelTests.cs
+++ tests/OLT.EF/OLT.EF.Common.Tests/EntityModelTests.cs
@@ -9,6 +9,34 @@ namespace OLT.EF.Common.Tests
     public class EntityModelTests
     {
         [Fact]
+        public void EntityIdModelTest()
+        {
+
+            var model = new EntityIdModel();
+            Assert.Equal(0, model.Id);
+            Assert.True(model.CreateDate < DateTimeOffset.Now.AddSeconds(2));
+            Assert.True(model.CreateDate > DateTimeOffset.Now.AddSeconds(-2));
+            Assert.Null(model.CreateUser);
+            Assert.Null(model.ModifyDate);
+            Assert.Null(model.ModifyUser);
+
+
+            model = EntityIdModel.FakerData();
+            Assert.NotEqual(0, model.Id);
+            Assert.NotEqual(DateTimeOffset.MinValue, model.CreateDate);
+            Assert.True(model.CreateDate < DateTimeOffset.Now);
+            Assert.NotNull(model.CreateUser);
+            Assert.NotNull(model.ModifyDate);
+            Assert.NotNull(model.ModifyUser);
+
+            Assert.IsAssignableFrom<IOltEntity>(model);
+            Assert.IsAssignableFrom<IOltEntityId>(model);
+            Assert.IsAssignableFrom<IOltEntityAudit>(model);
+            Assert.IsAssignableFrom<IOltEntityDeletable>(model);            
+            Assert.Throws<IsAssignableFromException>(() => Assert.IsAssignableFrom<IOltEntityDeletable>(model));
+        }
+
+        [Fact]
         public void EntityIdDeletableTest()
         {
             
@@ -67,6 +95,6 @@ namespace OLT.EF.Common.Tests
             Assert.Throws<IsAssignableFromException>(() => Assert.IsAssignableFrom<IOltEntityId>(model));            
             Assert.IsAssignableFrom<IOltEntityAudit>(model);
             Assert.IsAssignableFrom<IOltEntityDeletable>(model);
-        }   
+        }        
     }
 }
