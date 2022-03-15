6a6583713baa2b0f0396039be24e6ad32468f2af
Chris Straw <chrisstraw@users.noreply.github.com>
2022-03-15 14:08:19 -0400

    Moved OLT.EF.Core.SqlServer (#25)
    * Moved OLT.EF.Core.SqlServer

---
+++ src/OLT.EF.Core.SqlServer/Extensions/OltSqlModelBuilderHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLT.Core
+{
+    public static class OltSqlModelBuilderHelper
+    {
+        public static void SetIdentityColumns(ModelBuilder modelBuilder, int identitySeed, int identityIncrement)
+        {
+            if (modelBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(modelBuilder));
+            }
+
+            OltModelBuilderExtensions.EntitiesOfType<IOltEntityId>(modelBuilder, builder =>
+            {
+                var prop = builder.Property<int>(nameof(IOltEntityId.Id));
+                if (prop.Metadata.ValueGenerated == ValueGenerated.OnAdd && !prop.Metadata.GetIdentitySeed().HasValue)
+                {
+                    prop.UseIdentityColumn(identitySeed, identityIncrement);
+                }
+
+#pragma warning disable S125
+                //Console.WriteLine($"{builder.Metadata.GetTableName()} of type {builder.Metadata.ClrType.FullName} -> GetIdentitySeed: {prop.Metadata.GetIdentitySeed()} -> IdentitySeed: {IdentitySeed}{Environment.NewLine}");
+#pragma warning restore S125
+            });
+        }
+    }
+}
---
+++ tests/OLT.EF.Core.SqlServer.Tests/Assests/Entites/PersonEntity.cs
@@ -0,0 +1,15 @@
+﻿using OLT.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OLT.EF.Core.SqlServer.Tests.Assests.Entites
+{
+    public class PersonEntity : OltEntityIdDeletable
+    {
+        public string NameFirst { get; set; }
+        public string NameMiddle { get; set; }
+        public string NameLast { get; set; }
+    }
+
+}
