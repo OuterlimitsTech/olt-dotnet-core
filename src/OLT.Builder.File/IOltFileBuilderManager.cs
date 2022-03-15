db7cbcd2f80ae093b77e2e71bba6f443202515b9
Chris Straw <chris.straw@outerlimitstech.com>
2022-03-15 14:30:27 -0400

    Moved OLT.Builder.File & OLT.EPPlus

---
+++ src/OLT.Builder.File/IOltFileBuilderManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OLT.Core
+{
+    public interface IOltFileBuilderManager : IOltInjectableSingleton
+    {
+        List<IOltFileBuilder> GetBuilders();
+        IOltFileBase64 Generate<TRequest>(TRequest request, string name) where TRequest : IOltRequest;
+    }
+}
---
+++ tests/OLT.EPPlus.Tests/ExporterTests.cs
@@ -0,0 +1,24 @@
+using OfficeOpenXml;
+using OLT.EPPlus.Tests.Assets;
+using Xunit;
+
+namespace OLT.EPPlus.Tests
+{
+    public class ExporterTests
+    {
+
+        [Fact]
+        public void FileBuilder()
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.Commercial;
+
+            var list = PersonModel.FakerList(4);
+
+            var builder = new TestFileBuilder();
+            var result = builder.Build(new TestFileBuilderRequest { Data = list });
+
+            Assert.True(result.FileBase64.Length > 0);
+        }
+
+    }
+}
