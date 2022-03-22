cd586c0e72f72d13d48fb86fc8e688385b0f6000
Chris Straw <chris.straw@outerlimitstech.com>
2022-03-22 13:20:33 -0400

    Moved MaintainableAttribute

---
+++ src/OLT.Core.Services/CustomAttributes/Maintainable/OltMaintainableAttributeExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace OLT.Core
+{
+    public static class OltMaintainableAttributeExtensions
+    {
+
+        /// <summary>
+        /// Returns <see cref="MaintainableAttribute"/> attribute
+        /// </summary>
+        /// <param name="enum"></param>
+        /// <returns></returns>
+        public static MaintainableAttribute GetMaintainable(this Enum @enum)
+        {
+            return OltAttributeExtensions.GetAttributeInstance<MaintainableAttribute>(@enum) ?? new MaintainableAttribute();
+        }
+
+        /////// <summary>
+        /////// Returns <see cref="MaintainableAttribute"/> attribute
+        /////// </summary>
+        /////// <typeparam name="TEnum"></typeparam>
+        /////// <param name="enum"></param>
+        /////// <returns></returns>
+        ////public static MaintainableAttribute GetMaintainable<TEnum>(this TEnum @enum) where TEnum : System.Enum
+        ////{
+        ////    return OltAttributeExtensions.GetAttributeInstance<MaintainableAttribute, TEnum>(@enum) ?? new MaintainableAttribute();
+        ////}
+
+
+        /// <summary>
+        /// Sets the <see cref="IOltEntityMaintainable"/> fields using <see cref="MaintainableAttribute"/> attribute        
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="entity"></param>
+        /// <param name="enum"></param>
+        public static void SetMaintainable<TEntity, TEnum>(this TEntity entity, TEnum @enum)
+            where TEntity : class, IOltEntityMaintainable
+            where TEnum : System.Enum
+        {
+            GetMaintainable(@enum).SetEntityValues(entity);
+        }
+    }
+}
