ec03bc2bf6612c1f72b8815fbd65efc983b82aa0
Chris Straw <chris.straw@outerlimitstech.com>
2022-03-22 12:02:31 -0400

    moved OLT.EF.Core.SeedHelpers.Csv

---
+++ src/OLT.EF.Core.SeedHelpers.Csv/IOltCsvSeedModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OLT.Core
+{
+    public interface IOltCsvSeedModel<in TEntity>
+        where TEntity: IOltEntity, IOltEntityId
+    {
+        int Id { get; }
+        void Map(TEntity entity);
+    }
+}
---
+++ tests/OLT.EF.Core.SeedHelpers.Csv.Tests/Assets/PersonTypeCodeEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using OLT.Core;
+
+
+namespace OLT.EF.Core.SeedHelpers.Csv.Tests.Assets
+{
+    public class PersonTypeCodeEntity : IOltEntityCodeValue, IOltEntityId
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Code { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+
+    }
+}
