26663421d45065b955e51113e8bf05d002c642f1
Chris Straw <chris.straw@outerlimitstech.com>
2022-03-22 16:42:36 -0400

    Moved OLT.AspNetCore

---
+++ src/OLT.AspNetCore/Controllers/OltApiControllerBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// ReSharper disable once CheckNamespace
+namespace OLT.Core
+{
+    [ApiController]
+    [Produces("application/json")]
+    public abstract class OltApiControllerBase : ControllerBase
+    {
+
+        [NonAction]
+        public virtual BadRequestObjectResult BadRequest(string error)
+        {
+            return BadRequest(new OltErrorHttp { Message = error });
+        }
+
+        [NonAction]
+        public virtual OltInternalServerErrorObjectResult InternalServerError()
+        {
+            return new OltInternalServerErrorObjectResult();
+        }
+        [NonAction]
+        public virtual OltInternalServerErrorObjectResult InternalServerError(object value)
+        {
+            return new OltInternalServerErrorObjectResult(value);
+        }
+    }
+}
