fdfb3093fb81fac00daf0cd73e1463ce06da7e2f
Chris Straw <chris.straw@outerlimitstech.com>
2022-04-04 09:55:24 -0400

    closes #45

---
+++ src/OLT.Searchers.DateRange/Constants/DateRangeLabels.cs
@@ -0,0 +1,22 @@
+﻿namespace OLT.Constants
+{
+    public static class OltDateRangeLabels
+    {
+        public const string Today = "Today";
+        public const string Yesterday = "Yesterday";
+        public const string Tomorrow = "Tomorrow";
+        public const string Last7Days = "Last 7 Days";
+        public const string Next7Days = "Next 7 Days";
+        public const string ThisMonth = "This Month";
+        public const string LastMonth = "Last Month";
+        public const string NextMonth = "Next Month";
+        public const string MonthToDate = "MTD";
+        public const string ThisWeek = "This Week";
+        public const string NextWeek = "Next Week";
+        public const string YearToDate = "YTD";
+        public const string ThisYear = "This Year";
+        public const string LastYear = "Last Year";
+        public const string QuarterToDate = "QTD";
+        public const string PreviousQuarter = "Previous Quarter";
+    }
+}
