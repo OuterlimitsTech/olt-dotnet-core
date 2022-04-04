0b0838bfff422f9323aa0ebf29be35663f4d8c1c
Chris Straw <chris.straw@outerlimitstech.com>
2022-04-04 09:55:15 -0400

    Readme update

--- tests/OLT.Searchers.Tests/DateRangeSearcherTests.cs
+++ tests/OLT.Searchers.Tests/DateRangeSearcherTests.cs
@@ -24,7 +24,6 @@ namespace OLT.Searchers.Tests
             get
             {
                 var now = DateTimeOffset.Now;
-
                 var results = new TheoryData<FakeEntityDateRangeSearcher, OltDateRange, DateTimeOffset>();
                 results.Add(new FakeEntityDateRangeSearcher(), new OltDateRange(), DateTimeOffset.MinValue.AddSeconds(1));
                 results.Add(new FakeEntityDateRangeSearcher(now.Midnight(), now.Midnight().AddDays(3)), new OltDateRange(now.Midnight(), now.Midnight().AddDays(3)), now.Midnight().AddDays(3).AddSeconds(1));
