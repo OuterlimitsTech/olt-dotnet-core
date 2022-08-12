bc0d5c0a377b07bb52ebff821f09284e8e3ede92
Chris Straw <chrisstraw@users.noreply.github.com>
2022-08-12 12:28:15 -0400

    Nuget Updates - closes #75 (#76)

--- tests/OLT.Extensions.General.Tests/DoubleExtensionTests.cs
+++ tests/OLT.Extensions.General.Tests/DoubleExtensionTests.cs
@@ -13,7 +13,7 @@ namespace OLT.Extensions.General.Tests
         [InlineData(34, 34)]
         public void ToDollars(double value, double expectedResult)
         {
-            Assert.Equal(expectedResult, value.ToDollars());
+            Assert.Equal(expectedResult, OltDoubleExtensions.ToDollars(value));
         }
 
         [Theory]
@@ -21,11 +21,12 @@ namespace OLT.Extensions.General.Tests
         [InlineData(34.554, 34.55)]
         [InlineData(34.5545, 34.55)]
         [InlineData(34.5549, 34.55)]
-        [InlineData(34, 34)]
+        [InlineData(34d, 34d)]
         [InlineData(null, null)]
         public void ToDollarsNullable(double? value, double? expectedResult)
         {
-            Assert.Equal(expectedResult, value.ToDollars());
+            Assert.Equal(expectedResult, OltDoubleExtensions.ToDollars(value));
         }
+
     }
 }
