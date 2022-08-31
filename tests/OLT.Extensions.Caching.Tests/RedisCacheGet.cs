1c57ca689b35e404dcb4f15d7ee87d596e186551
Chris Straw <chris.straw@outerlimitstech.com>
2022-08-31 13:26:44 -0400

    code coverage

--- tests/OLT.Extensions.Caching.Tests/RedisCacheGet.cs
+++ tests/OLT.Extensions.Caching.Tests/RedisCacheGet.cs
@@ -111,50 +111,47 @@ namespace OLT.Extensions.Caching.Tests
 
 
 
-        //[Fact]
-        //public void FlushTests()
-        //{
+        [Fact]
+        public void FlushTests()
+        {
 
-        //    var cacheKeys = new Dictionary<string, OltPersonName>();
-        //    var services = new ServiceCollection();
-        //    var config = new RedisConfiguration
-        //    {
-        //        ConnectionString = $"{_config.RedisCacheConnectionString};database=3"
-        //    };
+            var cacheKeys = new Dictionary<string, OltPersonName>();
+            var services = new ServiceCollection();
+            var config = new RedisConfiguration
+            {
+                ConnectionString = $"{_config.RedisCacheConnectionString.Replace("unit-test", "unit-test-flush")},allowAdmin=true,defaultDatabase=4"
+            };
 
-        //    var provider = TestHelper.BuildRedisProvider(config, TimeSpan.FromMinutes(2), "flush-tests");
+            var provider = TestHelper.BuildRedisProvider(config, TimeSpan.FromMinutes(2), "flush-tests");
 
-        //    var cacheService = provider.GetRequiredService<IOltCacheService>();
+            var cacheService = provider.GetRequiredService<IOltCacheService>();
 
-        //    //for (var idx = 0; idx < 10; idx++)
-        //    //{
-        //    //    var model = TestHelper.CreateModel();
-        //    //    var cacheKey = $"cache-person-{Guid.NewGuid()}";
-        //    //    cacheKeys.Add(cacheKey, model);
-        //    //    oltMemoryCache.Get(cacheKey, () => TestHelper.CloneModel(model)).Should().BeEquivalentTo(model);
-        //    //    memoryCache.Get<OltPersonName>(cacheKey).Should().BeEquivalentTo(model);
-        //    //}
+            for (var idx = 0; idx < 10; idx++)
+            {
+                var model = TestHelper.CreateModel();
+                var cacheKey = $"cache-person-{Guid.NewGuid()}";
+                cacheKeys.Add(cacheKey, model);
+                cacheService.Get(cacheKey, () => TestHelper.CloneModel(model)).Should().BeEquivalentTo(model);
+            }
 
-        //    //foreach (var item in cacheKeys)
-        //    //{
-        //    //    var newModel = TestHelper.CreateModel();
-        //    //    var cacheKey = item.Key;
-        //    //    var expected = item.Value;
-        //    //    oltMemoryCache.Get(cacheKey, () => TestHelper.CloneModel(newModel)).Should().BeEquivalentTo(expected);
-        //    //    memoryCache.Get<OltPersonName>(cacheKey).Should().BeEquivalentTo(expected);
-        //    //}
+            foreach (var item in cacheKeys)
+            {
+                var newModel = TestHelper.CreateModel();
+                var cacheKey = item.Key;
+                var expected = item.Value;
+                cacheService.Get(cacheKey, () => TestHelper.CloneModel(newModel)).Should().BeEquivalentTo(expected);
+            }
 
-        //    //oltMemoryCache.Flush();
+            cacheService.Flush();
 
-        //    //foreach (var item in cacheKeys)
-        //    //{
-        //    //    var newModel = TestHelper.CreateModel();
-        //    //    var cacheKey = item.Key;
-        //    //    var expected = item.Value;
-        //    //    oltMemoryCache.Get(cacheKey, () => TestHelper.CloneModel(newModel)).Should().BeEquivalentTo(newModel);
-        //    //    memoryCache.Get<OltPersonName>(cacheKey).Should().BeEquivalentTo(newModel);
-        //    //}
-        //}
+            foreach (var item in cacheKeys)
+            {
+                var newModel = TestHelper.CreateModel();
+                var cacheKey = item.Key;
+                var expected = item.Value;
+                cacheService.Get(cacheKey, () => TestHelper.CloneModel(newModel)).Should().BeEquivalentTo(newModel);
+            }
+        }
 
         [Fact]
         public async Task FlushAsyncTests()
@@ -162,27 +159,15 @@ namespace OLT.Extensions.Caching.Tests
 
             var cacheKeys = new Dictionary<string, OltPersonName>();
             var services = new ServiceCollection();
-
             
             var config = new RedisConfiguration
             {
-                //ConnectionString = _config.RedisCacheConnectionString
-                ConnectionString = $"{_config.RedisCacheConnectionString.Replace("unit-test", "unit-test-flush")},allowAdmin=true,defaultDatabase=3"
+                ConnectionString = $"{_config.RedisCacheConnectionString.Replace("unit-test", "unit-test-flush-async")},allowAdmin=true,defaultDatabase=3"
             };
-
-            //config.Name = "unit-test-flush";
-            //config.Database = 3;
-            //config.AllowAdmin = true;
 
             var provider = TestHelper.BuildRedisProvider(config, TimeSpan.FromMinutes(2), "async-flush-tests");
 
             var cacheService = provider.GetRequiredService<IOltCacheService>();
-            var _redisFactory = provider.GetRequiredService<IRedisClientFactory>();
-
-            var redisDatabase = _redisFactory.GetDefaultRedisDatabase();
-            var test2 = _redisFactory.GetAllClients();
-            var test3 = _redisFactory.GetRedisDatabase();
-            var test = redisDatabase.Database;
 
             for (var idx = 0; idx < 10; idx++)
             {
