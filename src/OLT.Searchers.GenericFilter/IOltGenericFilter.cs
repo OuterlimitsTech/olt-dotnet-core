7343b2357eb74d63ff1d3889224789ddbc81bfe7
Chris Straw <chris.straw@outerlimitstech.com>
2022-09-26 10:45:56 -0400

    closes #85

--- src/OLT.Searchers.GenericFilter/IOltGenericFilter.cs
+++ src/OLT.Searchers.GenericFilter/IOltGenericFilter.cs
@@ -4,6 +4,7 @@
     {
         string Key { get; }
         bool HasValue(IOltGenericParameter parameters);
+        object GetValue(IOltGenericParameter parameters);
     }
 
     public interface IOltGenericFilter<TEntity> : IOltGenericFilter, IOltEntityQueryBuilder<TEntity>  
@@ -11,4 +12,5 @@
     {
 
     }
+
 }
