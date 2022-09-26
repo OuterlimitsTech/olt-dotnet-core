2ba5e6ca6826b5bc7f3afc68432c54c34f7b8367
Chris Straw <chrisstraw@users.noreply.github.com>
2022-09-26 12:09:10 -0400

    Generic Filter Mod to add GetValue (#86)
    * closes #85
    
    * Unit Test for Generic Filter Changes

--- src/OLT.Searchers.GenericFilter/OltGenericFilter.cs
+++ src/OLT.Searchers.GenericFilter/OltGenericFilter.cs
@@ -18,19 +18,30 @@ namespace OLT.Core
 
         
 
-        public bool HasValue(IOltGenericParameter parameters)
+        public virtual bool HasValue(IOltGenericParameter parameters)
         {
             var hasValue = Parser.Parse(parameters);
             QueryBuilder.Value = Parser.Value;
             return hasValue;
         }
 
-        public IQueryable<TEntity> BuildQueryable(IQueryable<TEntity> queryable)
+        public virtual object GetValue(IOltGenericParameter parameters)
+        {
+            var hasValue = Parser.Parse(parameters);
+            if (hasValue)
+            {
+                return Parser.Value;
+            }
+            return default(TValueType);
+        }
+
+        public virtual IQueryable<TEntity> BuildQueryable(IQueryable<TEntity> queryable)
         {
             if (!Parser.HasValue) return queryable;
             QueryBuilder.Value = Parser.Value;
             return QueryBuilder.BuildQueryable(queryable);
         }
+      
     }
 
 }
---
+++ tests/OLT.Searchers.Tests/GenericFilterTests.cs
@@ -0,0 +1,42 @@
+﻿using OLT.Constants;
+using OLT.Core;
+using OLT.Searchers.Tests.Assets;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace OLT.Searchers.Tests
+{
+    public class GenericFilterTests
+    {
+        [Theory]
+        [InlineData("Test", "Test", true)]
+        [InlineData("", null, false)]
+        [InlineData(" ", null, false)]
+        [InlineData(null, null, false)]
+        public void String(string value, string expected, bool parseable)
+        {
+            var entities = FakeEntity.FakerList(10).AsQueryable();
+            var random = Faker.RandomNumber.Next();
+            var key = $"key_{random}";
+            var label = $"label_{random}";
+            var template = new OltFilterTemplateString(key, label, false);
+            var expr = new OltEntityExpressionStringStartsWith<FakeEntity>(p => p.FirstName);
+            var genericParameter = TestHelper.BuildGenericParameter(key, value);
+            var filter = new OltGenericFilter<FakeEntity, string>(template, expr);
+            Assert.Equal(parseable, filter.HasValue(genericParameter));
+            Assert.Equal(expected, filter.GetValue(genericParameter));
+
+            if (parseable)
+            {
+                Assert.Empty(filter.BuildQueryable(entities).ToList());
+            }
+            else
+            {
+                Assert.NotEmpty(filter.BuildQueryable(entities).ToList());
+            }
+        }
+
+       
+    }
+}
