ff43890c67ae42e3944dfe0eaa7c1b0e2004aa53
Chris Straw <chris.straw@outerlimitstech.com>
2022-11-23 11:22:49 -0500

    closes #110

--- tests/OLT.DataAdapters.AutoMapper.Tests/Assets/Models/AdapterObject2.cs
+++ tests/OLT.DataAdapters.AutoMapper.Tests/Assets/Models/AdapterObject2.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace OLT.DataAdapters.AutoMapper.Tests.Assets.Models
 {
 
-    public class AdapterObject2
+    public class AdapterObject2 : IAdapterObject
     {
         public int ObjectId { get; set; }
         public OltPersonName Name { get; set; }
