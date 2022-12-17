cdb1733d6519cdf10aa1cda3ba4bfa983f2d21d4
Chris Straw <chris.straw@outerlimitstech.com>
2022-12-17 09:33:02 -0500

    Added Post Execute Methods
    closes #117

---
+++ src/OLT.Core.CommandBus/Records/OltAfterCommandQueueItem.cs
@@ -0,0 +1,10 @@
+﻿namespace OLT.Core
+{
+    public record OltAfterCommandQueueItem(IOltCommand Command, IOltCommandResult Result)
+    {
+        public static OltAfterCommandQueueItem Create(IOltCommand command, IOltCommandResult result)
+        {
+            return new OltAfterCommandQueueItem(command, result);
+        }
+    }
+}
