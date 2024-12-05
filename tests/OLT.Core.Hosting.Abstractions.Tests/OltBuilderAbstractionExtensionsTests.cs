51f090880f13abf37be3a734518d80821e008a37
Chris Straw <chris.straw@outerlimitstech.com>
2024-12-05 03:57:53 -0500

    app builder unit tests

--- tests/OLT.Core.Hosting.Abstractions.Tests/OltBuilderAbstractionExtensionsTests.cs
+++ tests/OLT.Core.Hosting.Abstractions.Tests/OltBuilderAbstractionExtensionsTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Diagnostics.CodeAnalysis;
@@ -11,7 +12,7 @@ public class OltBuilderAbstractionExtensionsTests
     {
         // Arrange
         var builder = new TestApplicationBuilder(Host.CreateApplicationBuilder());
-
+        
 
         // Assert
         var serviceProvider = builder.Services.BuildServiceProvider();
@@ -24,6 +25,42 @@ public class OltBuilderAbstractionExtensionsTests
         Assert.NotNull(oltIdentityConcrete);
         Assert.IsType<TestIdentity>(oltIdentity);
         Assert.IsType<TestIdentity>(oltDbAuditUser);
+    }
+
+    [Fact]
+    public void AddDevelopmentConfig_ShouldRegisterDevelopmentCorrectly()
+    {
+        // Arrange
+        var builder = new TestApplicationBuilder(Host.CreateApplicationBuilder());
+        builder.AddDevelopmentConfig<TestApplicationBuilder>(true);
+
+        // Assert
+        var serviceProvider = builder.Services.BuildServiceProvider();
+
+        var configuration = serviceProvider.GetRequiredService<IConfiguration>(); // as IConfigurationSource;
+        var root = (IConfigurationRoot)configuration;
+        var providers = root?.Providers?.Select(s => s.ToString()).ToList() ?? new List<string?>();
+
+        Assert.Contains("appsettings.Development.json", providers[^2]);
+        Assert.Contains("secrets.json", providers[^1]);
+    }
+
+    [Fact]
+    public void AddDevelopmentConfig_MissingRegisterDevelopmentCorrectly()
+    {
+        // Arrange
+        var builder = new TestApplicationBuilder(Host.CreateApplicationBuilder());
+        builder.AddDevelopmentConfig<TestApplicationBuilder>(false);
+
+        // Assert
+        var serviceProvider = builder.Services.BuildServiceProvider();
+
+        var configuration = serviceProvider.GetRequiredService<IConfiguration>(); // as IConfigurationSource;
+        var root = (IConfigurationRoot)configuration;
+        var providers = root?.Providers?.Select(s => s.ToString()).ToList() ?? new List<string?>();
+
+        Assert.DoesNotContain("appsettings.Development.json", providers[^2]);
+        Assert.DoesNotContain("secrets.json", providers[^1]);
     }
 
 
@@ -45,7 +82,7 @@ public class OltBuilderAbstractionExtensionsTests
 
         public override void AddServices()
         {
-            this.AddOltIdentity<IOltIdentity, TestIdentity>();
+            this.AddOltIdentity<IOltIdentity, TestIdentity>();            
         }
     }
 
